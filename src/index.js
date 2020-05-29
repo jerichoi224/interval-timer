edd17c2614bb0ed4dbd8885119ed355f87c9e279
Daniel Choi <jerichoi224@gmail.com>
2020-05-29 02:11:59 -0400

    initial

---
+++ src/index.js
@@ -0,0 +1,73 @@
+
+const button = document.getElementById('button');
+const timeText = document.getElementById('time');
+
+var seconds = 0
+var workTime = 50
+var breakTime = 10
+
+var running = false
+var work = false
+
+setInterval(incrementTime, 1000);
+
+button.onclick = e => {
+  // Start
+  if(!running){
+    seconds = 0
+    work = true
+    timeText.innerHTML = "00:00"
+    button.innerText = "Stop";
+    button.classList.remove('is-success');
+    button.classList.add('is-danger');
+  }
+
+  // Stop
+  else{
+    timeText.innerHTML = "00:00"
+    button.innerText = "Start";
+    button.classList.remove('is-danger');
+    button.classList.add('is-success');
+  }
+  running = !running
+}
+
+var convertTime = (sec) => {
+  let minutes = Math.floor(sec / 60);
+  let seconds = sec % 60;
+
+  minutes = String(minutes).padStart(2, "0");
+  seconds = String(seconds).padStart(2, "0");
+  return minutes + ":" + seconds;
+}
+
+function incrementTime() {
+  if(running){
+    seconds++;
+    timeText.innerHTML = convertTime(seconds)
+
+    if(work){
+      if(seconds === 60 * workTime){
+        work = false
+        seconds = 0
+  
+        let n = new Notification('Take a Break!', {
+          body: '50 Minutes Past.'
+        })          
+      }
+    }
+
+    if(!work){
+      if(seconds === 60 * breakTime){
+        work = false
+        seconds = 0
+
+        let n = new Notification('Times up!', {
+          body: 'Back to Work'
+        })        
+      }     
+    }
+
+    
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,95 @@
+const {app, BrowserWindow, Tray, nativeImage} = require('electron')
+const path = require('path')
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+    app.quit();
+  }
+
+let tray = undefined
+let window = undefined
+
+// This method is called once Electron is ready to run our code
+// It is effectively the main method of our Electron app
+app.on('ready', () => {
+  // Setup the menubar with an icon
+  let icon = nativeImage.createFromPath(path.join(__dirname, '/assets/icon.png'))
+  tray = new Tray(icon)
+
+  // Add a click handler so that when the user clicks on the menubar icon, it shows
+  // our popup window
+  tray.on('click', function(event) {
+    toggleWindow()
+
+    // Show devtools when command clicked
+    if (window.isVisible() && process.defaultApp && event.metaKey) {
+      window.openDevTools({mode: 'detach'})
+    }
+  })
+
+  // Make the popup window for the menubar
+  window = new BrowserWindow({
+    width: 250,
+    height: 60,
+    show: false,
+    frame: false,
+    resizable: false,
+    webPreferences: {
+        nodeIntegration: true,
+        enableRemoteModule: true
+      }
+  })
+//   window.webContents.openDevTools()
+
+  // Tell the popup window to load our index.html file
+  window.loadFile(path.join(__dirname, 'index.html'));
+
+
+  // Only close the window on blur if dev tools isn't opened
+  window.on('blur', () => {
+    if(!window.webContents.isDevToolsOpened()) {
+      window.hide()
+    }
+  })
+})
+
+const toggleWindow = () => {
+  if (window.isVisible()) {
+    window.hide()
+  } else {
+    showWindow()
+  }
+}
+
+const showWindow = () => {
+  const trayPos = tray.getBounds()
+  const windowPos = window.getBounds()
+  let x, y = 0
+  if (process.platform == 'darwin') {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height)
+  } else {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height * 10)
+  }
+
+  window.setPosition(x, y, false)
+  window.show()
+  window.focus()
+}
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+ 
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
